4cdeab1f68e2c2c0590c8f60f853469aff4d90d1
seunghoria <seungeun991114@gmail.com>
2022-05-04 15:30:19 +0900

    part2 _ stack queue

--- Comgong_bro/Part2/stack queue.py
+++ Comgong_bro/Part2/stack queue.py
@@ -10,12 +10,29 @@ while len(s) > 0:
 
 
 # 2. 큐 Queue
+
 from collections import deque
 
-q = deque()
-q.append(123)
-q.append(456)
-q.append(789)
-print("size:", len(q))
-while len(q) > 0:
-  print(q.popleft())
+dq = deque()
+dq.append(123)
+dq.appendleft(456)
+dq.appendleft(789)
+print(dq)
+
+print(dq.pop())
+print(dq.popleft())
+
+dq [789, 456, 123]
+
+from queue import Queue
+# 이 모듈은 멀티 스레드 환경을 위한 "thread-safe"기능으로
+# 실제 속도가 느려 쓸 필요가 없다. - 시간 초과 발생.
+q = Queue()
+q.put(123)
+q.put(456)
+q.put(789)
+while q:
+  print(q.get())
+  
+  
+  
