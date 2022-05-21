6a272aa6eb94f536b904a5a3edb72225bca422a3
seunghoria <seungeun991114@gmail.com>
2022-05-21 23:52:04 +0900

    (3)

--- Baekjoon/11286.py
+++ Baekjoon/11286.py
@@ -1,5 +1,12 @@
-import heapq
+import heapq as hq
+import sys
 
-N = int(input())
-for _ in range(N):
+input = sys.stdin.readline
+pq = []
+for _ in range(int(input())):
     x = int(input())
+    if x:
+        hq.heappush(pq,(abs(x), x)) # 튜플 (절대값, 원본값) - 우선순위 큐에 저장
+
+    else: # x = 0
+      print(hq.heappop(pq)[1] if pq else 0)
