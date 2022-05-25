074b17fe255fca24376165954e15dee901160651
seunghoria <seungeun991114@gmail.com>
2022-05-25 23:50:47 +0900

    4673

---
+++ Baekjoon/1546.py
@@ -0,0 +1,9 @@
+N = int(input())
+test_list = list(map(int, input().split()))
+max_score = max(test_list)
+
+new_list = []
+for score in test_list:
+    new_list.append(score/max_score * 100)
+test_avg = sum(new_list)/N
+print(test_avg)
